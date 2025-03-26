16dbc8769cf67a2f99d5e03b207a88b414d213e3
aninhah25 <136709743+aninhah25@users.noreply.github.com>
2025-03-26 14:52:25 -0300

    kkk

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
 document.addEventListener('DOMContentLoaded', function(){
-    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
-    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
-
     let tamanhoAtualFonte = 1;
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+   
     aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte += 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
-
     });
-
+    const diminuiFonteBotao= this.documentElementById ("diminuir-fonte");
     diminuiFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
-
-    });
-
-});
+    })
+    })
